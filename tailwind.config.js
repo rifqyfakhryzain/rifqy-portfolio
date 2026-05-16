21e4131ec5732f0587b528411293b7535e88f3a6
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2026-05-16 10:54:41 +0700

    feat: migrate from tailwind cdn to local tailwindcss v4

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./js/**/*.js"], // Membaca file index.html di folder utama
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
